1eadc1136b9b97517c810bbeae45d3a45c0dcabd
Caleb Kester <ckester@coachingactuaries.com>
2017-11-17 10:30:07 -0600

    Testing changes

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,8 @@
   Hover over this element
 </div>
 
+<div class="error">test</div>
+
 <div class="bg-white mx-auto max-w-sm shadow-lg rounded-lg overflow-hidden">
   <div class="sm:flex sm:items-center px-6 py-4">
     <img class="block h-16 sm:h-24 rounded-full mx-auto mb-4 sm:mb-0 sm:mr-4 sm:ml-0" src="https://avatars2.githubusercontent.com/u/4323180?s=400&u=4962a4441fae9fba5f0f86456c6c506a21ffca4f&v=4" alt="">
