e9990c7a4b440221bc814cd7cc3bb83061549a83
Caleb Kester <ckester@coachingactuaries.com>
2017-11-17 10:25:56 -0600

    forgetting some files

--- build/vue-loader.conf.js
+++ build/vue-loader.conf.js
@@ -1,8 +1,9 @@
-'use strict'
+"use strict";
 
-const utils = require('./utils')
-const config = require('../config')
-const isProduction = process.env.NODE_ENV === 'production'
+const utils = require("./utils");
+const config = require("../config");
+const isProduction = process.env.NODE_ENV === "production";
+const tailwindcss = require("tailwindcss");
 
 module.exports = {
   loaders: utils.cssLoaders({
@@ -12,9 +13,10 @@ module.exports = {
     extract: isProduction
   }),
   transformToRequire: {
-    video: 'src',
-    source: 'src',
-    img: 'src',
-    image: 'xlink:href'
-  }
-}
+    video: "src",
+    source: "src",
+    img: "src",
+    image: "xlink:href"
+  },
+  postcss: [tailwindcss("./tailwind-config.js"), require("autoprefixer")]
+};
