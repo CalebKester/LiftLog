6475a8250631564786d55d8cf978a385653bd3cf
Caleb Kester <ckester@coachingactuaries.com>
2017-11-17 10:25:43 -0600

    Getting cli and tailwind working

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <header>
+      <span>Vue.js PWA</span>
+    </header>
+    <main>
+      <img src="./assets/logo.png" alt="Vue.js PWA">
+			<div class="bg-blue hover:bg-purple text-center text-white font-semibold mx-auto px-4 py-2">
+  Hover over this element
+</div>
+
+<div class="bg-white mx-auto max-w-sm shadow-lg rounded-lg overflow-hidden">
+  <div class="sm:flex sm:items-center px-6 py-4">
+    <img class="block h-16 sm:h-24 rounded-full mx-auto mb-4 sm:mb-0 sm:mr-4 sm:ml-0" src="https://avatars2.githubusercontent.com/u/4323180?s=400&u=4962a4441fae9fba5f0f86456c6c506a21ffca4f&v=4" alt="">
+    <div class="text-center sm:text-left sm:flex-grow">
+      <div class="mb-4">
+        <p class="text-xl leading-tight">Adam Wathan</p>
+        <p class="text-sm leading-tight text-grey-dark">Developer at NothingWorks Inc.</p>
+      </div>
+      <div>
+        <button class="text-xs font-semibold rounded-full px-4 py-1 leading-normal bg-white border border-purple text-purple hover:bg-purple hover:text-white">Message</button>
+      </div>
+    </div>
+  </div>
+</div>
+      <router-view></router-view>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app"
+};
+</script>
+
+<style lang="scss">
+@import "./css/main.scss";
+</style>
